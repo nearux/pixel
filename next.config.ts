2e6ceea6d1e692b1208df9fedbd1670804bf387c
pmhxxhsj <minho7952@naver.com>
2025-10-24 22:30:56 +0900

    chore: build 환경 구축

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  trailingSlash: true,
+  output: "export",
 };
 
 export default nextConfig;
--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -5,6 +5,7 @@ import { PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { WagmiProvider } from "wagmi";
+
 import { config } from "@/shared/lib/wagmi";
 
 const client = new QueryClient();
