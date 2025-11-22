95aedac1b31234a2d483bd9e938353bb53a4db8a
pmhxxhsj <minho7952@naver.com>
2025-11-22 20:46:51 +0900

    feat: pinata 정의 및 API 정의

---
+++ src/app/api/files/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+import { pinata } from "@/shared/lib/pinata";
+
+export async function POST(request: NextRequest) {
+  try {
+    const formData = await request.formData();
+
+    const title = formData.get("title") as string;
+    const link = formData.get("link") as string;
+    const pixelIndex = formData.get("pixelIndex") as string;
+    const imageFile = formData.get("image")! as File;
+
+    const fileExtension = imageFile.name.split(".").pop() || "png";
+    const fileName = `${pixelIndex}_lasted.${fileExtension}`;
+    const renamedFile = new File([imageFile], fileName, {
+      type: imageFile.type,
+    });
+
+    const { cid: imageCid } = await pinata.upload.public.file(renamedFile);
+
+    const metadata = {
+      text: title,
+      link: link,
+      imageCid: imageCid.toString(),
+    };
+
+    const metadataBlob = new Blob([JSON.stringify(metadata)], {
+      type: "application/json",
+    });
+
+    const metadataFile = new File(
+      [metadataBlob],
+      `${pixelIndex}_metadata.json`
+    );
+    const { cid: metadataCid } = await pinata.upload.public.file(metadataFile);
+
+    return NextResponse.json(
+      { metadataCid: metadataCid.toString() },
+      { status: 200 }
+    );
+  } catch (e) {
+    console.error("Error uploading to Pinata:", e);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
